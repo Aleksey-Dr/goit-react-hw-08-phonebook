fda42e320503abef39a9b65784262421c2c7b9d8
Aleksey-Dr <113710928+Aleksey-Dr@users.noreply.github.com>
2023-06-22 09:48:29 +0000

    Deploying to gh-pages from @ Aleksey-Dr/goit-react-hw-08-phonebook@6bd7df4d2d30e6b11c69c0a6c8bb40f8c673a3d6 🚀

--- static/js/481.0c6d2b83.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[481],{5579:function(n,e,t){t.d(e,{Z:function(){return u}});var r=t(8182),c="Title_title__ugKfF",i=t(184),u=function(n){var e=n.children;return(0,i.jsx)("h2",{className:(0,r.Z)(c),children:e})}},1481:function(n,e,t){t.r(e),t.d(e,{default:function(){return i}});var r=t(5579),c=t(184),i=function(){return(0,c.jsx)("div",{children:(0,c.jsx)(r.Z,{children:"Welcome!"})})}}}]);
-//# sourceMappingURL=481.0c6d2b83.chunk.js.map
